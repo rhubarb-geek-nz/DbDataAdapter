f086ac6b235fa30d554e0280762bfc194c4699d6
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2025-03-23 21:41:15 +0000

    Added Read-DbCommand

---
+++ DbCommand.cs
@@ -0,0 +1,47 @@
+// Copyright (c) 2025 Roger Brown.
+// Licensed under the MIT License.
+
+using System;
+using System.Data.Common;
+using System.Management.Automation;
+
+namespace RhubarbGeekNz.DbDataAdapter
+{
+    [Cmdlet(VerbsCommunications.Read, "DbCommand")]
+    sealed public class ReadDbCommmand : PSCmdlet
+    {
+        [Parameter(Mandatory = true, HelpMessage = "Database Command")]
+        public DbCommand? Command;
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                if (Command == null)
+                {
+                    throw new PSArgumentNullException("Argument \"Command\" is null");
+                }
+
+                using (var reader = Command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int last = reader.FieldCount - 1;
+                        PSObject result = new PSObject();
+                        for (int i = 0; i < last; i++)
+                        {
+                            string name = reader.GetName(i);
+                            object? value = reader.IsDBNull(i) ? null : reader.GetValue(i);
+                            result.Members.Add(new PSNoteProperty(name, value));
+                        }
+                        WriteObject(result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, ex.GetType().Name, ErrorCategory.ReadError, null));
+            }
+        }
+    }
+}
